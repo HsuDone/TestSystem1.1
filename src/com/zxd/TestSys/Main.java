515fcb57281e2d92a975acd34ca8b6eab34c1d12
章旭东 <@DESKTOP-L7UF7LC>
2019-07-15 20:55:11 +0800

    JavaSE项目——考试系统

---
+++ src/com/zxd/TestSys/Main.java
@@ -0,0 +1,29 @@
+package com.zxd.TestSys;
+
+import com.zxd.TestSys.service.ClientContext;
+import com.zxd.TestSys.service.EntityContext;
+import com.zxd.TestSys.service.ExamService;
+import com.zxd.TestSys.ui.ExamFrame;
+import com.zxd.TestSys.ui.InfosFrame;
+import com.zxd.TestSys.ui.LoginFrame;
+import com.zxd.TestSys.ui.MenuFrame;
+import com.zxd.TestSys.ui.ResultFrame;
+import com.zxd.TestSys.ui.RulesFrame;
+
+public class Main {
+	public static void main(String[] args) {
+			LoginFrame loginFrame = new LoginFrame();
+			MenuFrame menuFrame = new MenuFrame();
+			ExamFrame examFrame = new ExamFrame();
+			EntityContext dataBase = new EntityContext();
+			RulesFrame rulesFrame = new RulesFrame();
+			InfosFrame infosFrame = new InfosFrame();
+			ResultFrame resultFrame = new ResultFrame();
+			//为服务注入依赖
+			ClientContext uiController = ClientContext.generateUiController(loginFrame,menuFrame,examFrame,rulesFrame,resultFrame,infosFrame);
+			ExamService backController = ExamService.generateExamService( uiController,dataBase);
+			//服务启动：为服务的窗口注入依赖
+			uiController.startWork();
+			backController.startWork();
+	}
+}
